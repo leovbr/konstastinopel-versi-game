3cc00e9cc32a65b808353a91ed471ef3c5378361
leovbr <leovbriansyh791@gmail.com>
2026-09-03 12:53:40 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+import { Game } from "./game.js";
+import { UI } from "./ui.js";
 
+const canvas=document.getElementById("game");
+const ui=new UI();
+const game=new Game(canvas,ui);
+
+document.getElementById("startBtn").addEventListener("click",()=>game.start());
+document.getElementById("restartBtn").addEventListener("click",()=>game.reset());
+
+for(const button of document.querySelectorAll("#controls button")){
+  button.addEventListener("click",()=>game.command(button.dataset.action));
+}
+
+game.run();
